8977ea17dc60f83a5852544da1af24d665313843
KillMonkey <1249082798@qq.com>
2016-09-03 22:49:51 +0800

    add project named Pone

---
+++ Pone/src/testOne.java
@@ -0,0 +1,20 @@
+
+public class testOne extends test{
+    
+//    public static void main(String[] args)
+//    {
+//    	test teClass=new test();
+//    	String aa= teClass.getStrOne();
+//    	System.out.println(aa);
+//    	System.out.println();
+//    }
+	public static void main(String[] args) {
+		//test.fyy();
+		//test otest = new test();
+		test otest1 = new test("wuxiao",123);
+		
+		int num = otest1.getNum();
+		System.out.println(num);
+	}
+    
+}
