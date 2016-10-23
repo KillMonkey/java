f8339f5d2c01b75396a24bd85d6c29fe8f123b46
KillMonkey <1249082798@qq.com>
2016-10-23 20:48:29 +0800

    serviceTest

---
+++ model/src/com/test/dm/ServiceTest.java
@@ -0,0 +1,17 @@
+package com.test.dm;
+
+import com.pattern.observe.ConcreteSubject;
+
+public class ServiceTest {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		ConcreteSubject oConcreteSubject = new ConcreteSubject();
+		System.out.println(oConcreteSubject);
+		@SuppressWarnings("unused")
+		ConcreteSubject oConcreteSubject2;
+		System.out.println();
+		System.out.println();
+	}
+
+}
